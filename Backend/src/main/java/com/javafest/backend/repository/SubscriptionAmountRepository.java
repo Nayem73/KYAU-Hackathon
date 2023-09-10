46a2698f0cad83feaa3f141b22d596974bf8dfb2
Nayem73 <nayem73@gmail.com>
2023-09-10 12:00:04 +0600

    hackathon backend

--- Backend/src/main/java/com/javafest/backend/repository/SubscriptionAmountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javafest.backend.repository;
-
-import com.javafest.backend.model.SubscriptionAmount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscriptionAmountRepository extends JpaRepository<SubscriptionAmount, Long> {
-}
